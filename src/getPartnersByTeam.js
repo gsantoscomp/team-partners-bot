be6db3661a409fc73734840c3c0247bd3fd6757a
d2p-guilherme <gsantos.comp@gmail.com>
2022-09-21 18:51:22 -0300

    Refatoração da busca de sócio-torcedores

---
+++ src/getPartnersByTeam.js
@@ -0,0 +1,51 @@
+import puppeteer from "puppeteer";
+import randomUserAgent from "random-useragent";
+
+export default async (teamsInformation) => {
+    const browser = await puppeteer.launch();
+    const DEFAULT_USER_AGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.75 Safari/537.36';
+
+    const teamsWithPartners = await Promise.all(
+        teamsInformation.map(async (team) => {
+            const userAgent = randomUserAgent.getRandom();
+            const UA = userAgent || DEFAULT_USER_AGENT;
+
+            const page = await browser.newPage();
+
+            await page.setUserAgent(UA);
+            await page.setRequestInterception(true);
+
+            page.on('request', (req) => {
+                if (req.resourceType() === 'image' || req.resourceType() === 'stylesheet' || req.resourceType() === 'font') {
+                    req.abort();
+                } else {
+                    req.continue();
+                }
+            });
+
+            let amountPartners = '';
+            if (team.hasApiCall) {
+                const response = await page.goto(team.apiLink);
+                const result = await response.json();
+
+                amountPartners = result.res.toString();
+            } else {
+                await page.goto(team.linkPartnersInformation);
+                const partners = await page.$(team.classToFind);
+                amountPartners = await partners.evaluate(el => el.textContent)
+            }
+
+            await page.close();
+
+            return {
+                team: team.name,
+                partners: amountPartners.replaceAll('.', '').trim()
+            };
+
+        })
+    );
+
+    await browser.close();
+
+    return teamsWithPartners;
+}
--- src/index.js
+++ src/index.js
@@ -1,50 +1,9 @@
 import teamsInformation from "./teamsInformation.js";
-import puppeteer from "puppeteer";
-import randomUserAgent from "random-useragent";
+import getPartnersByTeam from "./getPartnersByTeam.js";
 
-const browser = await puppeteer.launch();
-const DEFAULT_USER_AGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.75 Safari/537.36';
-
-const teamsByPartners = await Promise.all(
-    teamsInformation.map(async (team) => {
-        const userAgent = randomUserAgent.getRandom();
-        const UA = userAgent || DEFAULT_USER_AGENT;
-
-        const page = await browser.newPage();
-
-        await page.setUserAgent(UA);
-        await page.setRequestInterception(true);
-
-        page.on('request', (req) => {
-            if (req.resourceType() === 'image' || req.resourceType() === 'stylesheet' || req.resourceType() === 'font') {
-                req.abort();
-            } else {
-                req.continue();
-            }
-        });
-
-        let amountPartners = '';
-        if (team.hasApiCall) {
-            const response = await page.goto(team.apiLink);
-            const result = await response.json();
-
-            amountPartners = result.res.toString();
-        } else {
-            await page.goto(team.linkPartnersInformation);
-            const partners = await page.$(team.classToFind);
-            amountPartners = await partners.evaluate(el => el.textContent)
-        }
-
-        await page.close();
-
-        return {
-            team: team.name,
-            partners: amountPartners.replaceAll('.', '').trim()
-        };
-
-    })
-);
-
-await browser.close();
-
-console.log(teamsByPartners);
+try {
+    const partnersByTeam = await getPartnersByTeam(teamsInformation);
+    console.log(partnersByTeam);
+} catch (err) {
+    console.error(err);
+}
