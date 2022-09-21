681c5f158777c877065c731bd6eff8e258797e7c
d2p-guilherme <gsantos.comp@gmail.com>
2022-09-20 21:58:23 -0300

    Início do projeto

---
+++ src/teamsInformation.js
@@ -0,0 +1,57 @@
+export default [
+    {
+        "name": "atlético-mg",
+        "linkPartnersInformation": "https://galonaveia.atletico.com.br/home",
+        "classToFind": ".partners-count > p"
+    },
+    {
+        "name": "avaí",
+        "linkPartnersInformation": "https://socio-avai.futebolcard.com/",
+        "classToFind": "#torcedometro"
+    },
+    {
+        "name": "botafogo",
+        "linkPartnersInformation": "https://camisa7.botafogo.com.br/",
+        "hasApiCall": true,
+        "apiLink": "https://api.camisa7.botafogo.com.br/public/counter"
+    },
+    {
+        "name": "ceará",
+        "linkPartnersInformation": "https://www.sociovozao.com/",
+        "classToFind": "#wrapper > section > div > div.row.text-center.countTo > div:nth-child(1) > span > strong"
+    },
+    {
+        "name": "fortaleza",
+        "linkPartnersInformation": "https://sociofortaleza.com.br/",
+        "classToFind": ".main-somos > strong"
+    },
+    {
+        "name": "fluminense",
+        "linkPartnersInformation": "https://sociofutebol.com.br/",
+        "hasApiCall": true,
+        "apiLink": "https://api.sociofutebol.com.br/public/counter"
+    },
+    {
+        "name": "juventude",
+        "linkPartnersInformation": "https://sociojaconero.juventude.com.br/",
+        "hasApiCall": true,
+        "apiLink": "https://api.sociojaconero.com.br/public/counter"
+    },
+    {
+        "name": "palmeiras",
+        "linkPartnersInformation": "https://www.palmeiras.com.br/home/",
+        "classToFind": ".contSociosNum > b"
+    },
+    {
+        "name": "santos",
+        "linkPartnersInformation": "https://sociorei.com/",
+        "hasApiCall": true,
+        "apiLink": "https://api.sociorei.com/v2/public/counter"
+    },
+    {
+        "name": "são paulo",
+        "linkPartnersInformation": "https://sociotorcedor.com.br/",
+        "hasApiCall": true,
+        "apiLink": "https://api.sociotorcedor.com.br/public/counter"
+    }
+];
